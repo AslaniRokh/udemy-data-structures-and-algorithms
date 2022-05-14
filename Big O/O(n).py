32b4b0f3402a874f99a74d4e0b376773467a190b
Behnam Aslani Rokh <behnamaslani@protonmail.com>
2022-05-14 19:27:21 +0430

    Drop Constants

--- Big O/O(n).py
+++ Big O/O(n).py
@@ -3,3 +3,15 @@ def print_items(n):
         print(i)
 
 print_items(10)
+
+# Drop Constants
+def print_items_2(n):
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+
+print_items_2(10)
+# this ran n + n times or 2n
+# we can simplify O(2n) by Dropping the Constant, it doesn't matter if it's O(2n) or O(100n)
+# we Drop the Constant  and we just write it O(n)
